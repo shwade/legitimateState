fa702e93934fbe1763a54c6832fcda6a3efedebb
v.shmyrov <v.shmyrov@kt-team.de>
2020-06-19 16:17:48 +0400

    codestyle, update on a found bug

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Hello there, I am already Winston
 
 
 usage: php isReal.php -text 'sample text'
+
+Doesn't work:
+i am ideal => (((i))) (((am))) (((i)))deal
--- isReal.php
+++ isReal.php
@@ -1,7 +1,7 @@
 <?php
 
-preg_match_all('/(\w+)/',$argv[2], $matches);
-$res = preg_replace('/(' . implode('|', reset($matches)) . ')/i', "((($1)))", $argv[2]);
+preg_match_all('/(\w+)/u', $argv[2], $matches);
+$res = preg_replace('/(' . implode('|', reset($matches)) . ')/i', "((($0)))", $argv[2]);
 
 echo PHP_EOL . $res . PHP_EOL;
 
